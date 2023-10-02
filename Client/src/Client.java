905a124e1f0202ad984c59f23dbf7d8584a58e76
CodeOfUsami <zhang0285@foxmail.com>
2023-10-02 22:50:15 +0800

    finish:client

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -3,20 +3,25 @@
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import java.nio.file.*;
 
 public class Client
 {
     // Main method
     public static void main(String[] args)
     {
+
         String address ="127.0.0.1";
         int port = 8000;
         String localPath ="/rec";
-        String tarPath="/source/index.html";
+        String tarPath="/index.html";
         Scanner scanner = new Scanner(System.in);
+        System.out.println("client socket is running!");
         while(true){
+            System.out.print("expect command: ");
             String command = scanner.nextLine();
             String[] commandParts = command.split(" ");
+
             if(command=="") {
                 continue;
             }
@@ -33,72 +38,207 @@ public class Client
                         "  --address              Set the target IP address (default: 127.0.0.1).\n" +
                         "  --port                 Set the target port (default: 8000)\n"+
                         "  --local                Set the local receive path(default: /rec)\n"+
-                        "  --reqInfo              Set the target filePath(default: /source/index.html)\n"+
+                        "  --reqInfo              Set the target filePath(default: /index.html)\n"+
                         "  --sendReq              send request");
             }
             else if(commandParts[1].equals("--address")){
-                if(!commandParts[2].isEmpty())
-                address=commandParts[2];
+                if(!commandParts[2].isEmpty()) {
+                    address = commandParts[2];
+                    System.out.println("address has confirmed");
+                }
+                else {
+                    System.out.println("error:command not found");
+                }
                 //System.out.println("hello");
             }
             else if(commandParts[1].equals("--port")){
-                if(!commandParts[2].isEmpty())
-                port=Integer.getInteger(commandParts[2]);
+                if(!commandParts[2].isEmpty()) {
+                    port = Integer.getInteger(commandParts[2]);
+                    System.out.println("port has confirmed");
+                }
+                else {
+                    System.out.println("error:command not found");
+                }
                 //System.out.println("hello");
             }
             else if(commandParts[1].equals("--local")){
-                if(!commandParts[2].isEmpty())
-                localPath = commandParts[2];
+                if(!commandParts[2].isEmpty()) {
+                    localPath = commandParts[2];
+                    System.out.println("localPath has confirmed");
+                }
+                else {
+                    System.out.println("error:command not found");
+                }
                 //System.out.println("hello");
             }
             else if(commandParts[1].equals("--reqInfo")){
-                if(!commandParts[2].isEmpty())
-                tarPath = commandParts[2];
+                if(!commandParts[2].isEmpty()) {
+                    tarPath = commandParts[2];
+                    System.out.println("targetPath has confirmed");
+                }
+                else {
+                    System.out.println("error:command not found");
+                }
                 //System.out.println("hello");
             }
             else if(commandParts[1].equals("--sendReq")) {
-                try
-                {
-                    // Create a socket to connect to the server
-                    Socket connectToServer = new Socket("localhost", 8000);
+                try {
+//                    import java.io.*;
+//                    import java.net.Socket;
+//                    import java.nio.file.*;
+//
+//                    public class SaveHtmlToRecFolder {
+//                        public static void main(String[] args) {
+//                            String serverAddress = "localhost"; // 服务器地址
+//                            int serverPort = 8080; // 服务器端口号
+//                            String request = "GET /index.html HTTP/1.1\r\n" +
+//                                    "Host: " + serverAddress + "\r\n" +
+//                                    "\r\n"; // HTTP请求
+//                            String recFolder = "rec"; // 保存文件的目录
+//
+//                            try {
+//                                Socket socket = new Socket(serverAddress, serverPort);
+//                                System.out.println("已连接到服务器 " + serverAddress + ":" + serverPort);
+//
+//                                // 发送HTTP请求
+//                                OutputStream out = socket.getOutputStream();
+//                                out.write(request.getBytes("UTF-8"));
+//                                out.flush();
+//
+//                                // 读取服务器响应
+//                                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//                                StringBuilder response = new StringBuilder();
+//                                String line;
+//                                while ((line = in.readLine()) != null) {
+//                                    response.append(line).append("\n");
+//                                }
+//
+//                                // 关闭连接
+//                                socket.close();
+//
+//                                // 保存响应到本地文件
+//                                String htmlResponse = response.toString();
+//                                if (htmlResponse.contains("HTTP/1.1 200 OK")) {
+//                                    String filePath = Paths.get(recFolder, "index.html").toString();
+//                                    try {
+//                                        Files.write(Paths.get(filePath), htmlResponse.getBytes());
+//                                        System.out.println("文件已保存至 " + filePath);
+//                                    } catch (IOException e) {
+//                                        e.printStackTrace();
+//                                    }
+//                                } else {
+//                                    System.err.println("请求失败或文件不存在");
+//                                }
+//                            } catch (IOException e) {
+//                                e.printStackTrace();
+//                            }
+//                        }
+//                    }
 
+                    // Create a socket to connect to the server
+                    //System.out.print("Input the target file(default:/index.html): ");
+                    //tarPath = scanner.nextLine();
+//                    address = "127.0.0.1";
+//                    port = 8000;
+//                    localPath = "/rec";
+//                    tarPath ="/index.html";
+                    System.out.println("成功发送请求");
+
+                    String request = "GET " + tarPath + " HTTP/1.1\r\n" +
+                            "Host: " + address + "\r\n" +
+                            "\r\n"; // HTTP请求
+
+                    Socket connectToServer = new Socket(address, port);
+                    System.out.println("已连接到服务器 "+address+":port");
                     // Create a buffered input stream to receive data
                     // from the server
-                    BufferedReader isFromServer = new BufferedReader(
-                            new InputStreamReader(connectToServer.getInputStream()));
+
+//                    BufferedReader isFromServer = new BufferedReader(new InputStreamReader(connectToServer.getInputStream()));
 
                     // Create a buffered output stream to send data to the server
-                    PrintWriter osToServer =
-                            new PrintWriter(connectToServer.getOutputStream(), true);
+//                    PrintWriter osToServer = new PrintWriter(connectToServer.getOutputStream(), true);
 
                     // Continuously send radius and receive area
                     // from the server
-                    while (true)
-                    {
-                        // Read the radius from the keyboard
-                        //System.out.println("http request < IP | port | directory | target > { 127.0.0.1 | 7050 | ./rec } | ./source/index.html ");
-                        System.out.print("Please enter content: ");
-                        //Scanner scanner = new Scanner(System.in); // 创建一个Scanner对象，用于读取标准输入
-                        double radius = scanner.nextDouble();
 
-                        // Send the radius to the server
-                        osToServer.println(radius);
+//                    while (true)
+//                    {
+//                        // Read the radius from the keyboard
+//                        //System.out.println("http request < IP | port | directory | target > { 127.0.0.1 | 7050 | ./rec } | ./source/index.html ");
+//                        System.out.print("Please enter content: ");
+//                        //Scanner scanner = new Scanner(System.in); // 创建一个Scanner对象，用于读取标准输入
+//                        double radius = scanner.nextDouble();
+//
+//                        // Send the radius to the server
+//                        osToServer.println(radius);
+//
+//                        // Get area from the server
+//                        StringTokenizer st = new StringTokenizer(
+//                                isFromServer.readLine());
+//
+//                        // Convert string to double
+//                        double area = Double.valueOf(st.nextToken ());
+//
+//                        // Print area on the console
+//                        System.out.println("Area received from the server is "
+//                                +area);
+//                    }
+//                }
+//                catch (IOException ex)
+//                {
+//                    System.err.println(ex);
+//                }
+                    // 发送HTTP请求
+                    OutputStream out = connectToServer.getOutputStream();
+                    out.write(request.getBytes("UTF-8"));
+                    out.flush();
 
-                        // Get area from the server
-                        StringTokenizer st = new StringTokenizer(
-                                isFromServer.readLine());
+                    // 读取服务器响应
+                    BufferedReader in = new BufferedReader(new InputStreamReader(connectToServer.getInputStream()));
+                    StringBuilder response = new StringBuilder();
+                    String line;
 
-                        // Convert string to double
-                        double area = Double.valueOf(st.nextToken ());
+                    boolean headersCompleted = false;
 
-                        // Print area on the console
-                        System.out.println("Area received from the server is "
-                                +area);
+                    while ((line = in.readLine()) != null) {
+                        if (line.isEmpty() && !headersCompleted) {
+                            headersCompleted = true;
+                            continue;
+                        }
+                        if (headersCompleted) {
+                            response.append(line).append("\n");
+                        }
                     }
-                }
-                catch (IOException ex)
-                {
-                    System.err.println(ex);
+                    // 关闭连接
+                    connectToServer.close();
+
+
+                    // 保存响应到本地文件
+
+                    String htmlResponse = response.toString();
+                    //System.out.println(htmlResponse);
+                    if (!htmlResponse.isEmpty()) {
+
+                        String filePath = localPath+tarPath;
+                        //System.out.println(filePath);
+                        try {
+                            //Files.createDirectories(Paths.get(localPath));
+                            //System.out.println(filePath);
+                            //System.out.println(String.valueOf(Paths.get(filePath)));
+                            File file = new File("."+String.valueOf(Paths.get(filePath)));
+                            Files.write(Path.of("."+filePath), htmlResponse.getBytes("utf-8"));
+//                            FileWriter writer = new FileWriter("."+String.valueOf(Paths.get(filePath)));
+//                            writer.write(htmlResponse);
+                            //Files.write(Path.of(filePath), htmlResponse.getBytes());
+                            System.out.println("文件已保存至 " + filePath);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        System.err.println("请求失败或文件不存在");
+                    }
+                } catch (IOException e){
+                    e.printStackTrace();
                 }
             }
             else {
--- MultiThreadsServer/src/MultiThreadsServer.java
+++ MultiThreadsServer/src/MultiThreadsServer.java
@@ -43,6 +43,9 @@ public class MultiThreadsServer
                  */
                 // Print the new connect number on the console
                 //System.out.println("Starting thread "+i);
+                System.out.println("接受新连接:");
+                System.out.println("客户端地址: " + connectToClient.getInetAddress());
+                System.out.println("客户端端口: " + connectToClient.getPort());
                 /**
                  * 把对象传入子线程
                  */
@@ -97,6 +100,9 @@ class ThreadHandler extends Thread
             // Create data input and print streams
 //            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 //            OutputStream out = clientSocket.getOutputStream();
+            /**
+             * 初始化输入流和输出流
+             */
             BufferedReader in = new BufferedReader(new InputStreamReader(connectToClient.getInputStream()));
             OutputStream out = connectToClient.getOutputStream();
             // Continuously serve the client
@@ -179,7 +185,9 @@ class ThreadHandler extends Thread
 //            }
             String requestLine = in.readLine();
             System.out.println("客户端请求: " + requestLine);
-            if(requestLine.equals(null)) return;
+            if (requestLine == null) {
+                throw new NullPointerException("requestLine is null");
+            }
             String[] requestParts = requestLine.split(" ");
             if (requestParts.length >= 2) {
                 String requestedFile = requestParts[1];
@@ -189,6 +197,9 @@ class ThreadHandler extends Thread
                     // 读取文件内容并发送响应
                     byte[] fileContent = Files.readAllBytes(filePath);
                     String contentType = getContentType(filePath);
+                    /**
+                     * 构造回复报文
+                     */
                     String response = "HTTP/1.1 200 OK\r\n"
                             + "Content-Type: " + contentType + "\r\n"
                             + "Content-Length: " + fileContent.length + "\r\n"
@@ -196,11 +207,16 @@ class ThreadHandler extends Thread
                     out.write(response.getBytes("UTF-8"));
                     out.write(fileContent);
                     out.flush();
+                    System.out.println("回复成功");
                 } else {
+                    /**
+                     * 构造404报文
+                     */
                     // 文件不存在，发送404响应
                     String notFoundResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
                     out.write(notFoundResponse.getBytes("UTF-8"));
                     out.flush();
+                    System.err.println("对应文件不存在");
                 }
             }
 
@@ -208,9 +224,11 @@ class ThreadHandler extends Thread
             in.close();
             out.close();
         }
-        catch(IOException ex)
-        {
+        catch(IOException ex) {
             System.err.println(ex);
+        } catch (NullPointerException e) {
+            // 捕获 NullPointerException 并输出 "404"
+            System.err.println("对应文件不存在");
         }
     }
 }
